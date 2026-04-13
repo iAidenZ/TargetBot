4a788844f7e168788efbc76676a10be6aba95f48
iAidenZ <bensimo146@gmail.com>
2026-04-13 15:39:19 +0100

    disabled discord default help
    discord default help, blocking our custom !help comand

--- TargetBot.py
+++ TargetBot.py
@@ -95,7 +95,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
 
 @bot.event
